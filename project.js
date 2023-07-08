efe80f74f8c7f1b47e2785ddbe137c13dc0e311b
MD NOUMAN .  S <syednouman400.sn@gmail.com>
2023-07-08 10:46:08 +0530

    aa

--- project.js
+++ project.js
@@ -12,3 +12,13 @@ document.getElementById('back-to-top').addEventListener('click', function(event)
     event.preventDefault();
     window.scrollTo({ top: 0, behavior: 'smooth' });
 });
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+  
